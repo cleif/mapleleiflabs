870514791c8258c3a5955bbdce8f84c233bc608e
cleif <creighton.leif@gmail.com>
2025-02-17 14:41:28 -0600

    push my config file

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,21 +1,19 @@
 module.exports = {
   siteMetadata: {
     // Site URL for when it goes live
-    siteUrl: `https://elastic-meninsky-aa7c74.netlify.app/`,
+    siteUrl: `https://mapleleiflabs.github.io/`,
     // Your Name
-    name: 'Ryan Fitzgerald',
+    name: 'Creighton Leif',
     // Main Site Title
-    title: `Ryan Fitzgerald | Full-Stack Developer`,
+    title: `MapleLeif Labs | Azure, DevOps & Other Infrastructure Gotachas & Fixes`,
     // Description that goes under your name in main bio
-    description: `Lorem ipsum dolor sit amet consectetur adipisicing elit, ipsum.`,
     // Optional: Twitter account handle
-    author: `@rfitzio`,
     // Optional: Github account URL
-    github: `https://github.com/ryanfitzgerald`,
+    github: `https://github.com/cleif`,
     // Optional: LinkedIn account URL
-    linkedin: `https://www.linkedin.com/in/ryanafitzgerald/`,
+    linkedin: `https://www.linkedin.com/in/creightonleif/`,
     // Content of the About Me section
-    about: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus perferendis porro cumque ea error ab voluptatem. Temporibus adipisci exercitationem similique itaque quibusdam laudantium, qui molestiae quas, aut amet animi id.`,
+    about: `I've been in back-office IT since I was 16. I've done everyting from Help Desk, Client Engineering, VMware, Storage, Exchange & most recently Cloud (Azure).  I've experienced many 'weird' scenario's of which basic documentation doesn't always cover. I'll post about them here. Read it or don't. This is therapy for me :D`,
     // Optional: List your projects, they must have `name` and `description`. `link` is optional.
     projects: [
       {
@@ -40,36 +38,28 @@ module.exports = {
     // Optional: List your experience, they must have `name` and `description`. `link` is optional.
     experience: [
       {
-        name: 'Acme Corp',
-        description: 'Full-Stack Developer, February 2020 - Present',
-        link: 'https://github.com/RyanFitzgerald/devfolio',
+        name: 'Cloud Architect',
+        description: 'Cloud Architecture focusing on Core Services, Data, Security & Desktops',
       },
       {
-        name: 'Globex Corp',
-        description: 'Full-Stack Developer, December 2017 - February 2020',
-        link: 'https://github.com/RyanFitzgerald/devfolio',
+        name: 'Infrastructure Engineering',
+        description: 'Fortune 200 Manufacturing & Finance Industries doing VMware Infrastructure, Storage, DR & Exchange',
       },
       {
-        name: 'Hooli',
-        description: 'Full-Stack Developer, May 2015 - December 2017',
-        link: 'https://github.com/RyanFitzgerald/devfolio',
+        name: 'Help Desk, Client Engineering & System Administration',
+        description: '.Edu space for Jr. Colleges & Private Liberal Arts Colleges in the Midwest',
       },
     ],
     // Optional: List your skills, they must have `name` and `description`.
     skills: [
       {
-        name: 'Languages & Frameworks',
+        name: 'Infrastructure Platforms',
         description:
-          'JavaScript (ES6+), Golang, Node.js, Express.js, React, Ruby on Rails, PHP',
+          'Microsoft Azure, Windows Server, VMware, AKS, Databricks, Azure DevOps, Azure Virtual Desktop, Azure Front Door, Azure APIM',
       },
       {
-        name: 'Databases',
-        description: 'MongoDB, PostreSQL, MySQL',
-      },
-      {
-        name: 'Other',
-        description:
-          'Docker, Amazon Web Services (AWS), CI / CD, Microservices, API design, Agile / Scrum',
+        name: 'IaC Tooling',
+        description: 'Ansible, Terraform, PowerShell, Azure CLI, Bash',
       },
     ],
   },
